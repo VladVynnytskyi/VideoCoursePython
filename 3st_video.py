14cffd98472006d87477a02c2d805a3ce03ebeae
VladVynnytskyi <vynnytskyivladyslav@gmail.com>
2025-08-14 14:49:18 +0200

    3st_video.py done

---
+++ 3st_video.py
@@ -0,0 +1,15 @@
+#comments
+print("hi my friend", 53.3, "Hi", 345, sep = (", "), end = (" lol ")) # sep встроєний параметр для ком  або все що завгодно
+print("\n")
+print("\t hello") #типу абзац
+print('Jon\'s \\book') #\\ для виведення слешу
+
+print(5 - 3)
+print(6 % 4) #для остачі
+print(3**2) #для піднятя в степінь
+print(min(0, -2, 34, 5, 2))
+print(max(0, -2, 34, 5, 2))
+print(abs(-1))
+print(pow(3, 2))
+print(round(0.76))
+num = input("input your num ")

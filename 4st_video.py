7c938865703d71419cdaca91f1f1d9920e8e5a0f
VladVynnytskyi <vynnytskyivladyslav@gmail.com>
2025-08-15 10:08:51 +0200

    4st_video done

---
+++ 4st_video.py
@@ -0,0 +1,19 @@
+num = 5
+print(num)
+num = 7
+print("Num ", num)
+# del num
+# print (num)
+number = -123.234
+word = "asdfg"
+boolean = True #or False
+info = "Num "
+print(info , num)
+
+#Example
+
+a = int(input("Input your num")) #a = str(input("Input your num")) a = bool(input("Input your num"))
+b = input("Input your num")
+print(info, a + int(b))
+
+a += 5

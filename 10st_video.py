144b5e4d1ef0590e242462d7413375af26ce98bd
Vladyslav Vynnytskyi <vynnytskyivladyslav@gmail.com>
2025-08-20 12:09:53 +0200

    10st_video done

--- 10st_video.py
+++ 10st_video.py
@@ -20,9 +20,52 @@ print(person.items())         #це там де ключі і значення
 
 for key, values in person.items():
     print(key, values, sep = ' - ')         #як вивести ключ і аргумент
-
-for el in person.values():
-    print(el)                               #video course time code 11:00
-
-for el in person.keys():
+print('\n\n\n\n')
+for el in person.values():                      #ми тут виводимо тільки значення тобто values
+    print(el)                              
+print('\n\n\n\n\n')
+for el in person.keys():                    #ми тут будемо виводити виключно ключі
     print(el)                               
+
+print('\n\n\n\n\n')
+print(person.get('name'))
+print('\n\n\n')
+person.popitem()                #видаляє останній елемент
+print(person)
+
+person.pop(5)                   #видаляє елемент '5'
+
+print(person)
+
+person['bio'] = 'Text'          #додати новий елемент у кінець нашого словника
+
+print(person)
+
+
+
+person.clear()
+
+print (person)
+
+print('\n\n\n\n\n\n')
+
+
+#Створення великого словника
+
+people = {
+    'user_1' : {
+        'name' : 'John',
+        'age' : 16,
+        'address' : ("New York", 'Street', '562'),
+        'grades' : {'Math': 5, "Physics": 3}
+    },
+    'user_2' : {
+        'surname' : 'Vynnytskyi',
+        'name' : 'Vlad',
+        'age' : 18,
+        'adress' : ("New York", 'Street', '562'),
+        'grades' : {'Math': 5, "Physics": 5}
+    }
+}
+
+print(people['user_1']['address'][1])       #тут я відкриваю юзера 1 потім його адрес і тоді 1 індекс тобто вулиця

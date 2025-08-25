58f667a9275fee47ba3ad37ced531cc37bb021ee
Vladyslav Vynnytskyi <vynnytskyivladyslav@gmail.com>
2025-08-25 22:15:49 +0200

    19st_video

--- 19st_video.py
+++ 19st_video.py
@@ -2,3 +2,21 @@
 
 #Спадкування
 
+class build:
+    year = None
+    city = None
+    def __init__(self, year, city):
+        build.year = year
+        build.city = city
+        self.get_info()
+        
+    def get_info(self):
+        print('Year: ', self.year, ". City: ", self.city, sep=' ')
+         
+class school(build):                    #клас
+    pass 
+
+
+school = build(1999, 'Lviv')
+house = build(2001, "New York")
+shop = build( 2994, 'Kiyv')
